f312240fd6a806dc26e8f68fb063fab492a3fb7e
Adam Cardoso <adamjuniorcardoso@gmail.com>
2021-12-23 15:43:39 -0300

    Obtendo um usuário por id

--- src/main/java/com/adamcardoso/workshopmongo/services/UserService.java
+++ src/main/java/com/adamcardoso/workshopmongo/services/UserService.java
@@ -2,10 +2,13 @@ package com.adamcardoso.workshopmongo.services;
 
 import com.adamcardoso.workshopmongo.domain.User;
 import com.adamcardoso.workshopmongo.repository.UserRepository;
+import com.adamcardoso.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +18,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
