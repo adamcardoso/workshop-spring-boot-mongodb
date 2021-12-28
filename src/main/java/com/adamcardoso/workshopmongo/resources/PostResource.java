85a84ae9ef8b33b4dfe225b444f31056a744f61e
Adam Cardoso <adamjuniorcardoso@gmail.com>
2021-12-28 14:59:10 -0300

    Obtendo um post por id

---
+++ src/main/java/com/adamcardoso/workshopmongo/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.adamcardoso.workshopmongo.resources;
+
+import com.adamcardoso.workshopmongo.domain.Post;
+import com.adamcardoso.workshopmongo.domain.User;
+import com.adamcardoso.workshopmongo.dto.UserDTO;
+import com.adamcardoso.workshopmongo.services.PostService;
+import com.adamcardoso.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+
+        return ResponseEntity.ok().body(obj); // retorna as respostas http
+    }
+}
