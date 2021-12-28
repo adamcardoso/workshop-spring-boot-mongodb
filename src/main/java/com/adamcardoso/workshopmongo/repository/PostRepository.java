eb7fecd5d86fb6f1e03d669f31fd4a0b404f714f
Adam Cardoso <adamjuniorcardoso@gmail.com>
2021-12-28 15:24:19 -0300

    Consulta simples com @Query

--- src/main/java/com/adamcardoso/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/adamcardoso/workshopmongo/repository/PostRepository.java
@@ -2,11 +2,15 @@ package com.adamcardoso.workshopmongo.repository;
 
 import com.adamcardoso.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-    List<Post> findByTitleContaining(String text);
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
